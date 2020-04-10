fd3ca63078780a14b44dbfc17f84953ca6a3ed5f
Alvis Freddy Tuna Mamani <alvisfreddy@hotmail.com>
2020-04-10 13:02:16 -0400

    Inicio ( Realizando un Hola Mundo )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hola-mundo';
+	title = 'Hola Mundo en Angular !!!';
+
+	titulo = 'Manual de Angular de DesarrolloWeb.com';
+
+	visible = false;
+
+	decirAdios() {
+		this.visible = true;
+	}
 }
