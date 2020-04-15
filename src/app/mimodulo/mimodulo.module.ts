9739dbf5587eb1a69590206766cf620a01656f06
Alvis Freddy Tuna Mamani <alvisfreddy@hotmail.com>
2020-04-15 08:24:28 -0400

    Modulos ( Quitando mi-componente por posible confusión de nombres con otro componente )

--- src/app/mimodulo/mimodulo.module.ts
+++ src/app/mimodulo/mimodulo.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MiComponenteComponent } from './mi-componente/mi-componente.component';
-
 
 
 @NgModule({
 	declarations: [
-		MiComponenteComponent
 	],
 	imports: [
 		CommonModule
 	],
 	exports: [
-		MiComponenteComponent
 	]
 })
 export class MimoduloModule { }
