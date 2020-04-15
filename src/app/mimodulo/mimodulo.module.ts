f1fc17195ab0edc00c08e58c1c609363c39a6d29
Alvis Freddy Tuna Mamani <alvisfreddy@hotmail.com>
2020-04-15 12:08:08 -0400

    Directiva ( Agregando nuevo Componente para probar las directivas )

--- src/app/mimodulo/mimodulo.module.ts
+++ src/app/mimodulo/mimodulo.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MicomponenteComponent } from './micomponente/micomponente.component';
+import { DirectivasComponent } from './directivas/directivas.component';
 
 
 @NgModule({
 	declarations: [
-		MicomponenteComponent
+		MicomponenteComponent,
+		DirectivasComponent
 	],
 	imports: [
 		CommonModule
 	],
 	exports: [
-		MicomponenteComponent
+		MicomponenteComponent,
+		DirectivasComponent
 	]
 })
 export class MimoduloModule { }
