0d2be6107dd429da6b773044abb491a8b56444f1
Alvis Freddy Tuna Mamani <alvisfreddy@hotmail.com>
2020-04-21 14:03:01 -0400

    Directiva: ngFor ( Ejemplos )

--- src/app/mimodulo/directivas/directivas.component.ts
+++ src/app/mimodulo/directivas/directivas.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface PreguntasInterface {
+	pregunta: string;
+	si: number;
+	no: number;
+}
+
 @Component({
 	selector: 'app-directivas',
 	templateUrl: './directivas.component.html',
@@ -12,6 +18,38 @@ export class DirectivasComponent implements OnInit {
 	isUline;
 	isBorde;
 	isFondo;
+
+	// ngFor
+	preguntas: string[] = [
+		"¿España ganará la Euro 2016?",
+		"¿Hará sol el día de mi boda?",
+		"¿Estás aprendiendo Angular 2 en DesarrolloWeb?",
+		"¿Has hecho ya algún curso en EscuelaIT?"
+	];
+	preguntasObj = [
+		{
+			pregunta: "¿España ganará la Euro 2016?",
+			si: 22,
+			no: 95
+		},
+		{
+			pregunta: "¿Estás aprendiendo Angular 2 en DesarrolloWeb??",
+			si: 262,
+			no: 3
+		},
+		{
+			pregunta: "¿Has hecho ya algún curso en EscuelaIT??",
+			si: 1026,
+			no: 1
+		}
+	];
+	preguntasObj2: PreguntasInterface[] = [
+		{
+			pregunta: "¿Te gusta usar interfaces?",
+			si: 72,
+			no: 6
+		}
+	];
 
 	constructor() { }
 
