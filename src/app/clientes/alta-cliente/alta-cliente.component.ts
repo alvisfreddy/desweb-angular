6f0c604347487bc0d3bdda81543b6de9ad3683f0
Alvis Freddy Tuna Mamani <alvisfreddy@hotmail.com>
2020-04-22 13:29:46 -0400

    Clientes ( Agregando el servicio al componente )

--- src/app/clientes/alta-cliente/alta-cliente.component.ts
+++ src/app/clientes/alta-cliente/alta-cliente.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientesService } from "./../clientes.service";
+
 
 @Component({
 	selector: 'app-alta-cliente',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AltaClienteComponent implements OnInit {
 
-	constructor() { }
+	constructor(private clientesService: ClientesService) { }
 
 	ngOnInit(): void {
 	}
