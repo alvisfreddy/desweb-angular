cdc952369423a3ed40dd844a60e6236423c8ad14
Alvis Freddy Tuna Mamani <alvisfreddy@hotmail.com>
2020-04-22 12:43:10 -0400

    Clientes ( Agregando el servicio al módulo para utilizarlo en los componentes )

--- src/app/clientes/clientes.module.ts
+++ src/app/clientes/clientes.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { ClientesService } from "./clientes.service";
 
 
 @NgModule({
 	declarations: [],
 	imports: [
 		CommonModule
+	],
+	providers: [
+		ClientesService
 	]
 })
 export class ClientesModule { }
