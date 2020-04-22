e80ca6aa2e3c1d756404ce1e8e839f9a5f359f74
Alvis Freddy Tuna Mamani <alvisfreddy@hotmail.com>
2020-04-22 07:59:52 -0400

    Servicio ( Creando un servicio en mimodulo )

---
+++ src/app/mimodulo/miservicio.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+	providedIn: 'root'
+})
+export class MiservicioService {
+
+	datoServicio = "Alvis :) Infomración del Servicio";
+
+	constructor() { }
+}
