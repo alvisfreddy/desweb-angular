2807cc6d2bbdcb29872994e87cffafd5c9a68102
Alvis Freddy Tuna Mamani <alvisfreddy@hotmail.com>
2020-04-22 14:20:21 -0400

    Clientes : Listado ( Importando el Servicio y el Modelo de Datos )

--- src/app/clientes/listado-clientes/listado-clientes.component.ts
+++ src/app/clientes/listado-clientes/listado-clientes.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Cliente, Grupo } from "./../cliente.model";
+import { ClientesService } from "./../clientes.service";
+
 @Component({
 	selector: 'app-listado-clientes',
 	templateUrl: './listado-clientes.component.html',
