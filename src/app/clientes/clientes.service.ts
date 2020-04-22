011f5a5951338f4633703e4242b027e96fe3d578
Alvis Freddy Tuna Mamani <alvisfreddy@hotmail.com>
2020-04-22 10:50:29 -0400

    Clientes ( Inicializando los grupos y clientes )

--- src/app/clientes/clientes.service.ts
+++ src/app/clientes/clientes.service.ts
@@ -6,5 +6,16 @@ import { Cliente, Grupo } from './cliente.model';
 })
 export class ClientesService {
 
-	constructor() { }
+	private clientes: Cliente[];
+	private grupos: Grupo[];
+
+	constructor() {
+		this.grupos = [
+			{id: 0, nombre: 'Sin definir'},
+			{id: 1, nombre: 'Activos' },
+			{id: 2, nombre: 'Inactivos' },
+			{id: 3, nombre: 'Deudores' }
+		];
+		this.clientes = [];
+	}
 }
