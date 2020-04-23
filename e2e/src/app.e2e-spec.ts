89be2c24252008027c39c361666ff95286aa0011
Alvis Freddy Tuna Mamani <alvisfreddy@hotmail.com>
2020-04-23 11:42:21 -0400

    APP ( Renombrando nombre del proyecto )

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('hola-mundo app is running!');
+    expect(page.getTitleText()).toEqual('desweb-angular app is running!');
   });
 
   afterEach(async () => {
